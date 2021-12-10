ed82d42a31f13792cf9614dce81d747cfd1f9a42
ClaudiaCalero <claudiaclrduro@gmail.com>
2021-12-10 08:56:23 +0100

    hola

---
+++ src/components/router.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import App from "../App";
+import Create from "./create";
+
+
+function Router() {
+        return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App/>} />
+                    <Route path="/create" element={<Create/>} />
+                </Routes>  
+            </BrowserRouter>
+        )
+}
+
+export default Router;
